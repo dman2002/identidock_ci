d9ed88294d833190e7253080718d083a16336c96
Andrey.Dmitriev <adm291174@gmail.com>
2017-03-17 17:32:28 +0300

    Fixed security issues

--- app/identidock.py
+++ app/identidock.py
@@ -11,7 +11,7 @@ salt = "UNIQUE_SALT"
 def mainpage():
     name = default_name
     if request.method == 'POST':
-        name = request.form['name']
+        name = html.escape(request.form['name'], quote=True) 
     salted_name = salt + name
     name_hash = hashlib.sha256(salted_name.encode()).hexdigest() 
 
@@ -29,6 +29,7 @@ def mainpage():
 
 @app.route('/monster/<name>') 
 def get_identicon(name):
+    name = html.escape(name, quote=True)
     image = cache.get(name)
     if image is None:
         print ("Cache miss", flush=True)
